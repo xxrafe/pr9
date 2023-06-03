574a10bf70478ad01fd4fe06b356f94fe8708c0d
xxrafe <113291133+xxrafe@users.noreply.github.com>
2023-06-03 11:02:30 +0300

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Mi/Desktop/pr9-main
+# Build directory: C:/Users/Mi/Desktop/pr9-main/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
